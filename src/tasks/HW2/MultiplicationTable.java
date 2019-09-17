6d54325b6201b5a7bf951e15fc152d52753a78a7
obolos <mycontaktperson@gmail.com>
2019-09-18 00:13:17 +0300

    First Result

--- src/tasks/HW2/MultiplicationTable.java
+++ src/tasks/HW2/MultiplicationTable.java
@@ -1,5 +1,31 @@
 package tasks.HW2;
 
+import java.util.Scanner;
+
 // Необходимо вывести на экран таблицу умножения от 1 до 10 для введённого пользователем любого числа. Использовать циклы запрещено.
 public class MultiplicationTable {
+
+    private static void multiTableLoop(int userNum) {
+        if (userNum <= 10 && userNum > 0) {
+            for (int i = 1; i <= userNum; i++) {
+                System.out.println();
+                for (int  y = 1; y <= 10; y++) {
+                    System.out.println(i + " x " + y + " = " + i*y);
+                }
+            }
+        } else {
+            System.out.println("Вы вышли за пределы допустимых значений");
+        }
+    }
+
+      public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        if (sc.hasNextInt()) {
+           multiTableLoop(sc.nextInt());
+        } else {
+            System.out.println("Вы не ввели допустимое значение");
+        }
+
+    }
 }
--- src/tasks/HW3/BinaryFormat.java
+++ src/tasks/HW3/BinaryFormat.java
@@ -2,5 +2,19 @@ package tasks.HW3;
 
 // Ввести с консоли число в бинарном формате. Перевести его в десятичный формат, записать в переменную int и вывести на экран.
 
+import java.util.Scanner;
+
 public class BinaryFormat {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        try {
+            int x = Integer.parseInt(sc.nextLine(), 2);
+            System.out.println("Результатом будет " + x);
+        } catch (NumberFormatException e) {
+            //e.printStackTrace();
+            System.out.println("Введите число в бинарном формате прим. 101");
+        }
+    }
 }
