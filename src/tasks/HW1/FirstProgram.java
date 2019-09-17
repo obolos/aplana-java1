0c77b42bb8429b4bde5a0e2581e0b7587e8c5748
obolos <mycontaktperson@gmail.com>
2019-09-17 21:08:24 +0300

    Base Structure

---
+++ src/tasks/HW1/FirstProgram.java
@@ -0,0 +1,10 @@
+package tasks.HW1;
+
+// Перенесите скрипт в ваш файл программы и запустите созданную программу
+
+public class FirstProgram {
+        public static void main(String[] args) {
+            System.out.println("Hello, world!");
+
+    }
+}
